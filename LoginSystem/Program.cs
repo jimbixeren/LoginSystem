66eaa8d6c1b76e62972733585bbd18aa83207720
Ben Johnson <jimbixeren@gmail.com>
2022-02-04 20:18:59 +0100

    Add project files.

---
+++ LoginSystem/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace LoginSystem
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            ToolBar toolbar = new ToolBar();
+           
+
+
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = toolbar.MainMenu();
+            }
+        }
+
+       
+
+        
+    }
+
+   
+
+
+
+
+
+
+}
---
+++ LoginSystem/ToolBar.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem
+{
+    internal class ToolBar
+    {
+
+        public bool MainMenu()
+        //private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("====================");
+            Console.WriteLine("1) Reverse String");
+            Console.WriteLine("====================");
+            Console.WriteLine("2) Remove Whitespace");
+            Console.WriteLine("====================");
+            Console.WriteLine("3) Exit");
+            Console.WriteLine("====================");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    ReverseString();
+                    return true;
+                case "2":
+                    RemoveWhitespace();
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        private static string CaptureInput()
+        {
+            Console.Write("Enter the string you want to modify: ");
+            return Console.ReadLine();
+        }
+
+        private static void ReverseString()
+        {
+            Console.Clear();
+            Console.WriteLine("Reverse String");
+
+            char[] charArray = CaptureInput().ToCharArray();
+            Array.Reverse(charArray);
+            DisplayResult(String.Concat(charArray));
+        }
+
+        private static void RemoveWhitespace()
+        {
+            Console.Clear();
+            Console.WriteLine("Remove Whitespace");
+
+            DisplayResult(CaptureInput().Replace(" ", ""));
+        }
+
+        private static void DisplayResult(string message)
+        {
+            Console.WriteLine($"\r\nYour modified string is: {message}");
+            Console.Write("\r\nPress Enter to return to Main Menu");
+            Console.ReadLine();
+        }
+
+
+
+       
+    }
+
+
+}
+
